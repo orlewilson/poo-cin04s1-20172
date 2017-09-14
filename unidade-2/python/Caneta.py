9f46f5e721fb884ea68fd1f080e56040a5262b66
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2017-09-14 16:39:41 -0400

    Códigos em python visto na aula do dia 13/09/2017

--- unidade-2/python/Caneta.py
+++ unidade-2/python/Caneta.py
@@ -16,10 +16,57 @@ Nome do arquivo: Caneta.py
 # Classe Caneta
 class Caneta:
 
+	# atributos (sem encapsulamento)
+	#cor = None
+	#marca = None
+	#material = None
+	#tipo = None
+	#preco = None
+	#ponta = None
+	
+	# atributos (com encapsulamento)
+	# __ significa private do Java
+	__cor = None
+	__marca = None
+	__material = None
+	__tipo = None
+	__preco = None
+	__ponta = None
+
+	# Métodos para garantir o encapsulamento dos
 	# atributos
-	cor = None
-	marca = None
-	material = None
-	tipo = None
-	preco = None
-	ponta = None
+	def setCor(self, cor):
+		self.__cor = cor
+
+	def getCor(self):
+		return self.__cor
+
+	def setMarca(self, marca):
+		self.__marca = marca
+
+	def getMarca(self):
+		return self.__marca
+
+	def setMaterial(self, material):
+		self.__material = material
+
+	def getMaterial(self):
+		return self.__material
+
+	def setTipo(self, tipo):
+		self.__tipo = tipo
+
+	def getTipo(self):
+		return self.__tipo
+
+	def setPreco(self, preco):
+		self.__preco = preco
+
+	def getPreco(self):
+		return self.__preco
+
+	def setPonta(self, ponta):
+		self.__ponta = ponta
+
+	def getPonta(self):
+		return self.__ponta
--- unidade-2/python/Principal.py
+++ unidade-2/python/Principal.py
@@ -18,18 +18,34 @@ from Caneta import Caneta
 # criando objeto do tipo Caneta
 bic = Caneta()
 
-# atrituindo valores
-bic.cor = "vermelha"
-bic.marca = "BIC"
-bic.material = "plástico"
-bic.tipo = "esferográfica"
-bic.preco = 2.3
-bic.ponta = 0.7
+# atrituindo valores (sem encapsulamento)
+#bic.cor = "vermelha"
+#bic.marca = "BIC"
+#bic.material = "plástico"
+#bic.tipo = "esferográfica"
+#bic.preco = 2.3
+#bic.ponta = 0.7
 
-# mostrando valores
-print("Cor: " + bic.cor)
-print("Marca: " + bic.marca)
-print("Material: " + bic.material)
-print("Tipo: " + bic.tipo)
-print("Preço: " + str(bic.preco))
-print("Ponta: " + str(bic.ponta))
+# atrituindo valores (com encapsulamento)
+bic.setCor("vermelha")
+bic.setMarca("BIC")
+bic.setMaterial("plástico")
+bic.setTipo("esferográfica")
+bic.setPreco(2.3)
+bic.setPonta(0.7)
+
+# mostrando valores (sem encapsulamento)
+#print("Cor: " + bic.cor)
+#print("Marca: " + bic.marca)
+#print("Material: " + bic.material)
+#print("Tipo: " + bic.tipo)
+#print("Preço: " + str(bic.preco))
+#print("Ponta: " + str(bic.ponta))
+
+# mostrando valores (com encapsulamento)
+print("Cor: " + bic.getCor())
+print("Marca: " + bic.getMarca())
+print("Material: " + bic.getMaterial())
+print("Tipo: " + bic.getTipo())
+print("Preço: " + str(bic.getPreco()))
+print("Ponta: " + str(bic.getPonta()))
